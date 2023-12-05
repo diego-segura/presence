d6b01df0f93f396cf51a5e83c2aa5dc4999bfb60
diego <diegosegura@hey.com>
2023-12-05 00:34:04 -0500

    updated experimental flags

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
-  experimental: {
-    assets: true
-   },
   integrations: [tailwind()],
   adapter: vercel({
     imageService: true,
