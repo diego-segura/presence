73f214e9f7eae67a55265016e098ec8c41f55604
diego <diegosegura@hey.com>
2023-12-05 00:11:57 -0500

    adding tailwind, initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,13 @@
 import { defineConfig } from 'astro/config';
-// Use Vercel Edge Functions (Recommended)
 import vercel from '@astrojs/vercel/edge';
-// Can also use Serverless Functions
-// import vercel from '@astrojs/vercel/serverless';
-// Or a completely static build
-// import vercel from '@astrojs/vercel/static';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
   experimental: {
     assets: true
    },
+  integrations: [tailwind()],
   adapter: vercel({
     imageService: true,
   }),
