cb7c1cae74a585370461dc149e5a6839fc5e8149
diego <diegosegura@hey.com>
2024-06-30 18:45:39 -0400

    new article

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -192,6 +192,13 @@ declare module 'astro:content' {
   collection: "posts";
   data: InferEntrySchema<"posts">
 } & { render(): Render[".mdx"] };
+"2024-06-30.mdx": {
+	id: "2024-06-30.mdx";
+  slug: "2024-06-30";
+  body: string;
+  collection: "posts";
+  data: InferEntrySchema<"posts">
+} & { render(): Render[".mdx"] };
 };
 
 	};
